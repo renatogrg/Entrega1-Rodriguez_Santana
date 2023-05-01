48bb1d6f67afed83e69ff7f8f098e157fb498106
renatogrg <72505451@continental.edu.pe>
2023-05-01 12:18:49 -0500

    Actuzalización de la entrega 3

---
+++ inicio/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from inicio.models import Carro
+
+
+admin.site.register([Carro])
---
+++ inicio/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from inicio import views
+
+app_name = 'inicio'
+
+urlpatterns = [
+    path('', views.mi_inicio, name='inicio'),
+    path('crear-animal/', views.crear_carro, name='crear_carro'),
+    path('animales/', views.lista_carros, name='listar_carros'),
+    
+]   
